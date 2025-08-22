6b736d6dd0eb04598c0777c4ba30e826811ddc9b
JIMENENDEZ25 <jmenendeza3@miumg.edu.gt>
2025-08-21 20:59:15 -0600

    server

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var corsOptions = {
     origin: "http://localhost:8081"
 };
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
